8c70f47f4832c0112325568ac88a39d27ad54edf
Zach Dubuc <zach.dubuc@gmail.com>
2020-01-31 20:42:21 -0500

    Edited player, discard my accidental camera changes

---
+++ GGJ2020/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 m_Direction;
+    private float m_Speed;
+    private float m_Velocity;
+
+    public CharacterController pController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Direction = Vector3.zero;
+        m_Velocity = 0;
+        m_Speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            m_Direction += Vector3.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_Direction += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            m_Direction += Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_Direction += Vector3.right;
+        }
+
+        pController.Move(m_Direction * m_Speed * Time.deltaTime);
+    }
+}
