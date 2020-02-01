16ce68b090316d01efcf88f9602f4b075832c0ce
Zach Dubuc <zach.dubuc@gmail.com>
2020-02-01 16:48:05 -0500

    commit boi

--- GGJ2020/Assets/Scripts/PlayerScripts/Player.cs
+++ GGJ2020/Assets/Scripts/PlayerScripts/Player.cs
@@ -11,8 +11,8 @@ public class Player : MonoBehaviour
     private Vector3 m_Velocity = new Vector3();
     public float m_Speed;
     public Camera pCamera;
-    private float m_Gravity = 9.4f;
-    Vector3 lastDirection = new Vector3();
+    private float m_Gravity = 14f;
+    Vector3 lastVelocity = new Vector3();
     public CharacterController pController;
     const float SPEED = 8f;
     float m_DashBoost = 0f;
@@ -21,9 +21,10 @@ public class Player : MonoBehaviour
     const float DASH_TIMER = 3f;
     //Power up stats
     public float m_AttackPower = 1f;
-    public int m_Health = 3;
+    public int m_Health = 100;
     public bool m_Armour = false;
     public float m_SpeedBoost = 0f;
+
 
 
     State m_CurrentState;
@@ -32,7 +33,8 @@ public class Player : MonoBehaviour
     {
         Moving,
         Dashing,
-        Idle
+        Idle,
+        Jumping
     };
     // Start is called before the first frame update
     void Start()
@@ -45,96 +47,177 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //get the cameras forward vector
-        Vector3 camForward = pCamera.transform.forward;
-        camForward.y = 0;
-        camForward = Vector3.Normalize(camForward);
- 
-        
-        switch (m_CurrentState)
-        {
-            case State.Idle:
-                Debug.Log("Idle state");
-                m_Velocity = Vector3.zero;
-                getInput();
-                break;
-
-            case State.Moving:
-                pController.transform.forward = Vector3.Slerp(pController.transform.forward, m_Direction, Time.deltaTime);
-                pController.transform.forward = m_Direction;
-                Vector3 dir = Vector3.zero; 
-               // m_Direction = Vector3.zero;
-                if (InputManager.getDashDown())
-                {
-                    Dash();
-                }
-                if (InputManager.getMoveForward())
-                {
-                    dir += camForward * 1;
-                }
-                if (InputManager.getMoveLeft())
-                {
-                    dir += (pCamera.transform.right * -1);
-                }
-                if (InputManager.getMoveBack())
-                {
-                    dir += (camForward * -1);
-                }
-                if (InputManager.getMoveRight())
-                {
-                    dir += pCamera.transform.right * 1;
-                }
-                dir = Vector3.Normalize(dir);
-                
-                if (dir == Vector3.zero)
-                {
-                    m_CurrentState = State.Idle;
-                }
-                m_Direction = dir;
-                lastDirection = m_Direction;
-                m_Velocity = m_Direction * (m_Speed + m_SpeedBoost + m_DashBoost);
-                break;
-            case State.Dashing:
-                
-                if (m_DashBoost > 0)
-                {
-                    m_DashBoost -= 2f;
-                    if (m_DashBoost < 0)
-                    {
-                        m_DashBoost = 0;
-                    }
-                }
-                else
-                {
-                    m_CurrentState = State.Moving;
-                    Debug.Log("Setting state to Moving");
-                }
-                m_Velocity = m_Direction * (m_Speed + m_SpeedBoost + m_DashBoost);
-                break;
-        }
-        
-        if (dashTimer > 0)
-        {
-            dashTimer -= Time.deltaTime;
-        }
-        
         if (isGrounded())
         {
-            m_Velocity.y = 0;
+            //get the cameras forward vector
+            Vector3 camForward = pCamera.transform.forward;
+            camForward.y = 0;
+            camForward = Vector3.Normalize(camForward);
+            if(lastVelocity != Vector3.zero)
+            {
+                pController.transform.forward = Vector3.Slerp(pController.transform.forward, m_Direction, Time.deltaTime);
+                pController.transform.forward = m_Direction;
+            }
+            
+            Vector3 dir = Vector3.zero;
+            // m_Direction = Vector3.zero;
+            if (InputManager.getDashDown() && isGrounded())
+            {
+                Dash();
+            }
+            if (InputManager.getMoveForward())
+            {
+                dir += camForward * 1;
+            }
+            if (InputManager.getMoveLeft())
+            {
+                dir += (pCamera.transform.right * -1);
+            }
+            if (InputManager.getMoveBack())
+            {
+                dir += (camForward * -1);
+            }
+            if (InputManager.getMoveRight())
+            {
+                dir += pCamera.transform.right * 1;
+            }
+            if(dir != Vector3.zero)
+            {
+                m_Direction = dir;
+                m_Velocity = m_Direction * (m_Speed + m_SpeedBoost + m_DashBoost);
+            }
+            else
+            {
+                m_Velocity = Vector3.zero;
+            }
+            
+            if (InputManager.getJumpDown())
+            {
+                m_Velocity.y += 5.5f;
+            }
         }
         else
         {
             m_Velocity.y -= m_Gravity * Time.deltaTime;
         }
-        
-        
-        pController.Move(m_Velocity * Time.deltaTime);
-    }
+        if (m_DashBoost > 0)
+        {
+            m_DashBoost -= 2f;
+            if (m_DashBoost < 0)
+            {
+                m_DashBoost = 0;
+            }
+        }
+        if (dashTimer > 0)
+        {
+            dashTimer -= Time.deltaTime;
+        }
 
+
+        /*switch (m_CurrentState)
+        {
+            //Idle state so player doesn't rotate
+            case State.Idle:
+                getInput();
+                break;
+
+            case State.Moving:
+                if (isGrounded())
+                {
+                    pController.transform.forward = Vector3.Slerp(pController.transform.forward, m_Direction, Time.deltaTime);
+                    pController.transform.forward = m_Direction;
+                    Vector3 dir = Vector3.zero;
+                    // m_Direction = Vector3.zero;
+                    if (InputManager.getDashDown() && isGrounded())
+                    {
+                        Dash();
+                    }
+                    if (InputManager.getMoveForward())
+                    {
+                        dir += camForward * 1;
+                    }
+                    if (InputManager.getMoveLeft())
+                    {
+                        dir += (pCamera.transform.right * -1);
+                    }
+                    if (InputManager.getMoveBack())
+                    {
+                        dir += (camForward * -1);
+                    }
+                    if (InputManager.getMoveRight())
+                    {
+                        dir += pCamera.transform.right * 1;
+                    }
+
+                    dir = Vector3.Normalize(dir);
+                    
+                    m_Direction = dir;
+                    m_Velocity = m_Direction * (m_Speed + m_SpeedBoost + m_DashBoost);
+                    if(Vector3.Distance(m_Velocity,lastVelocity) < 0.2)
+                    {
+                        m_CurrentState = State.Idle;
+                    }
+                    if (InputManager.getJumpDown())
+                    {
+                        jump();
+                    }
+                }
+                break;
+            case State.Dashing:
+                if (isGrounded())
+                {
+
+
+                    if (m_DashBoost > 0)
+                    {
+                        m_DashBoost -= 2f;
+                        if (m_DashBoost < 0)
+                        {
+                            m_DashBoost = 0;
+                        }
+                    }
+                    else
+                    {
+                        m_CurrentState = State.Moving;
+                        Debug.Log("Setting state to Moving");
+                    }
+                    m_Velocity = m_Direction * (m_Speed + m_SpeedBoost + m_DashBoost);
+                }
+                break;
+            case State.Jumping:
+                if(!isGrounded())
+                {
+                    m_Velocity.y -= m_Gravity * Time.deltaTime;
+                   // m_Velocity.y -= lastVelocity.y;
+                }
+                else
+                {
+                    m_Velocity.y = 0;
+                    m_CurrentState = State.Moving;
+                }
+                break;
+        } */
+
+        pController.Move(m_Velocity * Time.deltaTime);
+
+        
+        lastVelocity = m_Velocity;
+    }
+    public void jump()
+    {
+        if(isGrounded())
+        {
+            m_Velocity.y = 0;
+            m_Velocity.y += 7.5f;
+            m_CurrentState = State.Jumping;
+        }
+        
+    }
     public bool isGrounded()
     {
-
-       if( Physics.Raycast(transform.position, Vector3.down, 0.4f))
+       // Ray ray = new Ray(transform.position, Vector3.down);
+       // Debug.DrawRay(transform.position, new Vector3(0,-1.5f,0),Color.red);
+       if( Physics.Raycast(transform.position, Vector3.down, 1.4f))
         {
             return true;
         }
@@ -143,11 +226,11 @@ public class Player : MonoBehaviour
 
     public void getInput()
     {
-        if((InputManager.getMoveForward() == true || InputManager.getMoveLeft() == true || InputManager.getMoveRight() == true || InputManager.getMoveBack() == true) && m_CurrentState != State.Dashing)
+        if ((InputManager.getMoveForward() == true || InputManager.getMoveLeft() == true || InputManager.getMoveRight() == true || InputManager.getMoveBack() == true) && m_CurrentState != State.Dashing)
         {
             m_CurrentState = State.Moving;
         }
-        else if (InputManager.getDashDown())
+        else if (InputManager.getDashDown() && isGrounded())
         {
             if(m_Direction == Vector3.zero)
             {
@@ -158,8 +241,11 @@ public class Player : MonoBehaviour
             }
             Dash();
         }
-        else
+        else if(InputManager.getJumpDown())
         {
+            jump();
+        }
+        else {
             m_Velocity = Vector3.zero;
             m_CurrentState = State.Idle;
         }
@@ -191,7 +277,12 @@ public class Player : MonoBehaviour
 
     }
 
-    public void OnCollisionEnter(Collision collision)
+    public void addJumpHeight(float amount)
+    {
+
+    }
+
+   /* public void OnCollisionEnter(Collision collision)
     {
         GameObject obj = collision.gameObject;
 
@@ -199,6 +290,6 @@ public class Player : MonoBehaviour
         {
 
         }
-    }
+    } */
 
 }
--- GGJ2020/Assets/Scripts/PlayerScripts/PlayerCamera.cs
+++ GGJ2020/Assets/Scripts/PlayerScripts/PlayerCamera.cs
@@ -14,16 +14,18 @@ public class PlayerCamera : MonoBehaviour
     public Transform _cameraPivot;
     public Transform _cameraTarget;
     public float _lerpAmount = 1.75f;
+    public float _pivotLerp = 11.0f;
 
     public float _mouseSensitivity = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
+        _cameraPivot.transform.position = _cameraPivot.transform.position + ((_player.transform.position - _cameraPivot.transform.position)
+            * _pivotLerp * Time.deltaTime);
+
         transform.position = transform.position + ((_cameraTarget.position - transform.position) * _lerpAmount * Time.deltaTime);
-        transform.forward = _player.position - transform.position;
-
-
+        transform.forward = transform.forward + ((_player.position - transform.position) * _lerpAmount * Time.deltaTime);
 
         Vector3 pivot = _cameraPivot.rotation.eulerAngles;
         pivot.x = 0.0f;
@@ -33,6 +35,6 @@ public class PlayerCamera : MonoBehaviour
         _cameraPivot.rotation = Quaternion.Euler(pivot);
         
 
-        _cameraPivot.transform.position = _player.transform.position;
+        
     }
 }
