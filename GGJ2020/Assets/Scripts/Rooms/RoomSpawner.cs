f2758bf0e0fdbefce172394993c0d8b1d89f2ebf
Kris <creepyfridge@gmail.com>
2020-02-02 15:13:34 -0500

    jobs room can spawn

--- GGJ2020/Assets/Scripts/Rooms/RoomSpawner.cs
+++ GGJ2020/Assets/Scripts/Rooms/RoomSpawner.cs
@@ -14,6 +14,8 @@ public class RoomSpawner : MonoBehaviour
             spawnRoom();
         } while (_rooms.Count < _numRoomsInFloor);
 
+        spawnJobsRoom();
+
         removeDoors();
     }
 
@@ -28,7 +30,79 @@ public class RoomSpawner : MonoBehaviour
     public List<Room> _rooms;
     public GameObject _pivot;
     public List<GameObject> _roomPrefabs;
+    public GameObject _jobsRoom;
     public int _numRoomsInFloor;
+
+
+    void spawnJobsRoom()
+    {
+        bool isColliding = false;
+        GameObject pivot = null;
+        int roomId = -1;
+        int wallID = -1;
+        int doorID = -1;
+        Room newRoom = null;
+        int newWallID = -1;
+        int newDoorID = -1;
+        do
+        {
+            //pick spawnDoor
+            roomId = pseudoRandomID();
+            wallID = _rooms[roomId].getPotentialWall();
+            doorID = _rooms[roomId]._walls[wallID].getPotentialDoor();
+
+            if (pivot == null)
+            {
+                //pick new Room
+                GameObject newRoomObject = GameObject.Instantiate(_jobsRoom);
+                newRoomObject.transform.rotation = Quaternion.Euler(Vector3.zero);
+
+                newRoom = newRoomObject.GetComponentInChildren<Room>();
+
+                newWallID = newRoom.getPotentialWall();
+                newDoorID = newRoom._walls[newWallID].getPotentialDoor();
+
+                pivot = GameObject.Instantiate(_pivot);
+
+
+                //move pivot to door
+                pivot.transform.position = newRoom._walls[newWallID]._doors[newDoorID].transform.position;
+                //make room child of pivot
+                newRoomObject.transform.parent = pivot.transform;
+            }
+            //place/rotate the room
+
+            for (int i = 0; i < 360; i += 90)
+            {
+                pivot.transform.rotation = Quaternion.Euler(new Vector3(0.0f, (float)i, 0.0f));
+                pivot.transform.position = _rooms[roomId]._walls[wallID]._doors[doorID].transform.position;
+                newRoom.calculateFootPrint();
+                isColliding = false;
+
+                for (int j = 0; j < _rooms.Count && !isColliding; j++)
+                {
+                    if (newRoom.isColliding(_rooms[j]))
+                    {
+                        isColliding = true;
+                    }
+                }
+
+                if (!isColliding)
+                    break;
+            }
+
+            if (isColliding)
+            {
+                _rooms[roomId]._walls[wallID]._doors[doorID].hideModel();
+            }
+        }
+        while (isColliding);
+
+        _rooms.Add(newRoom);
+        _rooms[roomId].setActiveDoor(wallID, doorID);
+        newRoom.setActiveDoor(newWallID, newDoorID);
+        sortRooms();
+    }
 
     void spawnRoom()
     {
@@ -94,13 +168,10 @@ public class RoomSpawner : MonoBehaviour
         }
         while (isColliding);
 
-        
-        {
-            _rooms.Add(newRoom);
-            _rooms[roomId].setActiveDoor(wallID, doorID);
-            newRoom.setActiveDoor(newWallID, newDoorID);
-            sortRooms();
-        }
+        _rooms.Add(newRoom);
+        _rooms[roomId].setActiveDoor(wallID, doorID);
+        newRoom.setActiveDoor(newWallID, newDoorID);
+        sortRooms();
     }
     void sortRooms()
     {
