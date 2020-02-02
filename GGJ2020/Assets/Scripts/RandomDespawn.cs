8b298f72520f5f7d973043f078c54dda85605bf2
Kris <creepyfridge@gmail.com>
2020-02-02 12:26:33 -0500

    scene changes and random de spawn script

---
+++ GGJ2020/Assets/Scripts/RandomDespawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDespawn : MonoBehaviour
+{
+    public float _percentChanceToDespawn = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.Range(0.0f, 100.0f) < _percentChanceToDespawn)
+            gameObject.SetActive(false);
+
+    }
+}
