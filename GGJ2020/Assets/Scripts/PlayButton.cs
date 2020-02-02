3cd989d34ab2f27f907ebf89dfbb085363e6bf38
Kris <creepyfridge@gmail.com>
2020-02-02 14:55:53 -0500

    main menu done

---
+++ GGJ2020/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Play();
+    }
+}
